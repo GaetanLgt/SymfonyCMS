6e1b2ff8dc3083997c3a99f5665976b0368b3b58
GaetanLgt <gtn.langlet@gmail.com>
2023-01-18 05:29:40 +0100

    set category display menu

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\Category;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -10,10 +11,13 @@ class CategoryController extends AbstractController
 {
     // display category by slug take one slug parms 
     #[Route('/category/{slug}', name: 'category_show')]
-    public function show(): Response
+    public function show(?Category $category): Response
     {
+        if(!$category){
+            return $this->redirectToRoute('app_home');
+        }
         return $this->render('category/show.html.twig', [
-            'controller_name' => 'CategoryController',
+            'category' => $category,
         ]);
     }
 }
